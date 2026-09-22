035aa77e2a51f7cca4aee4506fbbe1d05351053e
Areia Ana <portalcore.consult@gmail.com>
2026-09-22 12:18:36 -0300

    Ajustar capitalização do Painel Diretoria Topcon BI

--- assets/js/portal.js
+++ assets/js/portal.js
@@ -39,7 +39,7 @@ if(!list.some((r)=>r.codigo==='ind_controle_caixas')){
 
 const painelDiretoria=list.find((r)=>r.codigo==='ind_painel_diretoria_topcon_bi');
 if(painelDiretoria){
-  painelDiretoria.titulo='PAINEL DIRETORIA TOPCON BI';
+  painelDiretoria.titulo='Painel Diretoria Topcon BI';
   painelDiretoria.subtitulo='Zoho Analytics';
   painelDiretoria.url='https://analytics.zoho.com/workspace/1762700000040012018/view/1762700000230550749';
   painelDiretoria.target='_blank';
@@ -48,7 +48,7 @@ if(painelDiretoria){
   list.push({
     module_id:indicadores.id,
     codigo:'ind_painel_diretoria_topcon_bi',
-    titulo:'PAINEL DIRETORIA TOPCON BI',
+    titulo:'Painel Diretoria Topcon BI',
     subtitulo:'Zoho Analytics',
     url:'https://analytics.zoho.com/workspace/1762700000040012018/view/1762700000230550749',
     target:'_blank',
